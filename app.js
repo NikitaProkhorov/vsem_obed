5639a0393934dbe583d7eb9ca770dd552f322904
NikitaProkhorov <118209171+NikitaProkhorov@users.noreply.github.com>
2023-12-18 05:51:14 +0500

    Update app.js

--- app.js
+++ app.js
@@ -7,78 +7,78 @@ tg.MainButton.color = '#2cab37';
 
 let item = "";
 
-let btn1 = document.getElementById("btn1");
-let btn2 = document.getElementById("btn2");
-let btn3 = document.getElementById("btn3");
-let btn4 = document.getElementById("btn4");
-let btn5 = document.getElementById("btn5");
-let btn6 = document.getElementById("btn6");
+// let btn1 = document.getElementById("btn1");
+// let btn2 = document.getElementById("btn2");
+// let btn3 = document.getElementById("btn3");
+// let btn4 = document.getElementById("btn4");
+// let btn5 = document.getElementById("btn5");
+// let btn6 = document.getElementById("btn6");
 
-btn1.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 1!");
-		item = "1";
-		tg.MainButton.show();
-	}
-});
+// btn1.addEventListener("click", function(){
+// 	if (tg.MainButton.isVisible) {
+// 		tg.MainButton.hide();
+// 	}
+// 	else {
+// 		tg.MainButton.setText("Вы выбрали товар 1!");
+// 		item = "1";
+// 		tg.MainButton.show();
+// 	}
+// });
 
-btn2.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 2!");
-		item = "2";
-		tg.MainButton.show();
-	}
-});
+// btn2.addEventListener("click", function(){
+// 	if (tg.MainButton.isVisible) {
+// 		tg.MainButton.hide();
+// 	}
+// 	else {
+// 		tg.MainButton.setText("Вы выбрали товар 2!");
+// 		item = "2";
+// 		tg.MainButton.show();
+// 	}
+// });
 
-btn3.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 3!");
-		item = "3";
-		tg.MainButton.show();
-	}
-});
+// btn3.addEventListener("click", function(){
+// 	if (tg.MainButton.isVisible) {
+// 		tg.MainButton.hide();
+// 	}
+// 	else {
+// 		tg.MainButton.setText("Вы выбрали товар 3!");
+// 		item = "3";
+// 		tg.MainButton.show();
+// 	}
+// });
 
-btn4.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 4!");
-		item = "4";
-		tg.MainButton.show();
-	}
-});
+// btn4.addEventListener("click", function(){
+// 	if (tg.MainButton.isVisible) {
+// 		tg.MainButton.hide();
+// 	}
+// 	else {
+// 		tg.MainButton.setText("Вы выбрали товар 4!");
+// 		item = "4";
+// 		tg.MainButton.show();
+// 	}
+// });
 
-btn5.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 5!");
-		item = "5";
-		tg.MainButton.show();
-	}
-});
+// btn5.addEventListener("click", function(){
+// 	if (tg.MainButton.isVisible) {
+// 		tg.MainButton.hide();
+// 	}
+// 	else {
+// 		tg.MainButton.setText("Вы выбрали товар 5!");
+// 		item = "5";
+// 		tg.MainButton.show();
+// 	}
+// });
 
-btn6.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 6!");
-		item = "6";
-		tg.MainButton.show();
-	}
-});
+// btn6.addEventListener("click", function(){
+// 	if (tg.MainButton.isVisible) {
+// 		tg.MainButton.hide();
+// 	}
+// 	else {
+// 		tg.MainButton.setText("Вы выбрали товар 6!");
+// 		item = "6";
+// 		tg.MainButton.show();
+// 	}
+// });
 
 
 function grab(g) {
@@ -95,54 +95,8 @@ function grab(g) {
 
 
 function openNewPage(url) {
-    window.location.href = url;
+    window.location.href = 'prod.html';
 }
-
-
-
-
-let initialX = null;
-
-function touchStart(event) {
-    initialX = event.touches[0].clientX;
-}
-
-function touchMove(event) {
-    if (initialX === null) {
-        return;
-    }
-
-    const currentX = event.touches[0].clientX;
-    const diffX = initialX - currentX;
-
-    event.currentTarget.scrollLeft += diffX;
-    initialX = currentX;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
